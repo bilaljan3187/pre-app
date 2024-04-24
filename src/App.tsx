95b38175438232fc7535c95bf994af6c58a07146
bilaljan3187 <bilaljan318718@gmail.com>
2024-04-24 23:39:23 +0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='mb-4'>
+        Hello world !
+      </h1>
+      <p>Bilal Muhammad</p>
+    </>
+  )
+}
+
+export default App
