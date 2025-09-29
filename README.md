6ab2eb24ec26f504242ce079a63094c17969f0df
bilaljan3187 <bilaljan318718@gmail.com>
2025-09-30 00:07:38 +0400

    initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#practical react essentials
+
+this is my first app for react for the course
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="mb-4">Hello World</h1>
+      <p>Bilal Muhammad </p>
+    </>
+  );
+}
+
+export default App;
